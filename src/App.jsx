5a59e0b4484ca3c0f9b4c5f566f8c3d6ad67aa65
mavlonmirzaxalilov <mavlonjonmirzaxalilov@gmail.com>
2025-10-23 16:48:23 +0500

    app.jsx configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import NavBar from "./components/NavBar.jsx";
+import Product from "./components/Product.jsx";
+import Hero from "./components/Hero.jsx";
 
 const App = () => {
     return (
         <main>
             <NavBar/>
+            <Hero/>
+            <Product/>
         </main>
     )
 }
