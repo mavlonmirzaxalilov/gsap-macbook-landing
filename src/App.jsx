1e5dc00ca9ad0be38050c1559951bf49d99415d7
mavlonmirzaxalilov <mavlonjonmirzaxalilov@gmail.com>
2025-10-23 11:42:01 +0500

    add 3d model

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
+import Product from "./components/Product.jsx";
 
 const App = () => {
     return (
         <main>
             <NavBar/>
+            <Hero/>
+            <Product/>
         </main>
     )
 }
