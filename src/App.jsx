22cbe5db883feb5aa5788dcac8b30fe4e942d45d
mavlonmirzaxalilov <mavlonjonmirzaxalilov@gmail.com>
2025-10-24 13:19:42 +0500

    Model change and color change work

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import NavBar from "./components/NavBar.jsx";
 import Product from "./components/Product.jsx";
 import Hero from "./components/Hero.jsx";
+import gsap from 'gsap'
+import {ScrollTrigger} from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
     return (
--- src/components/models/Macbook.jsx
+++ src/components/models/Macbook.jsx
@@ -9,9 +9,9 @@ Title: macbook pro M3 16 inch 2024
 */
 
 import React from 'react'
-import {useGLTF, useTexture} from '@react-three/drei'
+import {useGLTF} from '@react-three/drei'
 
-export function Model(props) {
+export default function MacbookModel(props) {
     const {nodes, materials} = useGLTF('/models/macbook-transformed.glb')
 
     return (
